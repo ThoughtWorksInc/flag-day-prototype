e664ddfe49f6e0f1b2193d6c5bad601c3dacd145
Stephanie Siaw <ssiaw@thoughtworks.com>
2015-10-22 15:35:09 +0800

    Basic page layout, jade templating added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var app = express()
 
+app.set('view engine', 'jade');
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Flag Day Check In'});
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
